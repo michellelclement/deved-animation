86b120fafdf4f44070d3e20c3d9c48c6127b3a5e
Michelle Clement <michellelclement@gmail.com>
2022-06-01 14:42:44 +0000

    Animation done now with close button working

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ tl.fromTo(
   "<50%"
 );
 tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  gsap.to(".cookie-container", {
+    opacity: 0,
+    y: 100,
+    duration: 0.75,
+    ease: "power1.out"
+  });
+});
